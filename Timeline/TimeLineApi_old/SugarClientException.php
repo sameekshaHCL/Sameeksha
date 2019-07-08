2e86976299727916f109a718825e5cca1faa0e0b
sameekshaHCL <sameeksha.agrawal@hcl.com>
2019-07-08 13:00:48 +0530

    Internal_Functionality_Review

---
+++ Timeline/TimeLineApi_old/SugarClientException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+*	All Sugar Client Exception should be writtien in here
+*/
+class Unauthenticate_SugarClientException extends Exception{
+
+}
+class Failauthenticate_SugarClientException extends Exception{
+
+}
+?>
